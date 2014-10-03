cda37e091c4ee8ec154d944835add66061a75d22
Honlan <493722771@qq.com>
2014-10-03 13:12:02 +0800

    first edition

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 header("Content-type:text/html;charset=gbk");
 //define your token
 define("TOKEN", "omnilab");
+
 $wechatObj = new wechatCallbackapiTest();
 $wechatObj->responseMsg();
 
+
 class wechatCallbackapiTest
-{
+{   
     public function responseMsg()
     {
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
@@ -58,6 +60,7 @@ class wechatCallbackapiTest
                     $mysql->runSql($sql);*/
 
                     $post_string = '%7B%22records%22%3A%20%5B%7B%22OpenID%22%3A%20%22'.$fromUsername.'%22%2C%20%22发布时间%22%3A%20%22'.date('Y-m-d H:i',time()).'%22%2C%20%22评论内容%22%3A%20%22'.$keyword.'%22%7D%5D%2C%20%22force%22%3A%20true%2C%20%22method%22%3A%20%22insert%22%2C%20%22resource_id%22%3A%20%2261aab5bb-e5e2-455c-a4eb-77f504df1ce3%22%7D';
+                    //$post_string = "{'records':[{'OpendID':".$fromUsername.",'发布时间':".date('Y-m-d H:i',time()).",'评论内容':".$keyword."}],'force':true,'method':'insert','resource_id':'61aab5bb-e5e2-455c-a4eb-77f504df1ce3'}";
                     $remote_server = 'http://202.121.178.242/api/3/action/datastore_upsert';
                     $context = array(
                         'http'=>array(
@@ -68,7 +71,7 @@ class wechatCallbackapiTest
                     $stream_context = stream_context_create($context);
                     $data = file_get_contents($remote_server,FALSE,$stream_context);
 
-                    $contentStr = $fromUsername.' '.$time.' '.' '.$keyword;
+                    $contentStr = $fromUsername.' '.$time.' '.$toUsername.' '.$keyword;
                     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, "text", $contentStr);
                     echo $resultStr;
 
@@ -130,8 +133,7 @@ class wechatCallbackapiTest
             $mysql->closeDb();
 
         }else {
-            echo "";
-            exit;
+            
         }
     }
 
