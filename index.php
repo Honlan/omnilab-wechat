1590865e2d72869ff13a563cbc890d49c5d2a690
Honlan <493722771@qq.com>
2014-10-06 23:02:35 +0800

    finished

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 header("Content-type:text/html;charset=gbk");
 //define your token
 define("TOKEN", "omnilab");
+date_default_timezone_set(PRC);
 
 $wechatObj = new wechatCallbackapiTest();
 $wechatObj->responseMsg();
@@ -21,8 +22,10 @@ class wechatCallbackapiTest
             $toUsername = $postObj->ToUserName;
             $time = time();
             $msgType = $postObj->MsgType;
+
+            $first = true;
             
-            $mysql = new SaeMysql();
+            //$mysql = new SaeMysql();
             
             $textTpl = "<xml>
             <ToUserName><![CDATA[%s]]></ToUserName>
@@ -59,19 +62,26 @@ class wechatCallbackapiTest
                     /*$sql = "update user_state set state = '1' where fromUsername = '$fromUsername'";
                     $mysql->runSql($sql);*/
 
-                    $post_string = '%7B%22records%22%3A%20%5B%7B%22OpenID%22%3A%20%22'.$fromUsername.'%22%2C%20%22发布时间%22%3A%20%22'.date('Y-m-d H:i',time()).'%22%2C%20%22评论内容%22%3A%20%22'.$keyword.'%22%7D%5D%2C%20%22force%22%3A%20true%2C%20%22method%22%3A%20%22insert%22%2C%20%22resource_id%22%3A%20%2261aab5bb-e5e2-455c-a4eb-77f504df1ce3%22%7D';
-                    //$post_string = "{'records':[{'OpendID':".$fromUsername.",'发布时间':".date('Y-m-d H:i',time()).",'评论内容':".$keyword."}],'force':true,'method':'insert','resource_id':'61aab5bb-e5e2-455c-a4eb-77f504df1ce3'}";
-                    $remote_server = 'http://202.121.178.242/api/3/action/datastore_upsert';
-                    $context = array(
-                        'http'=>array(
-                            'method'=>'POST',
-                            'header'=>'Authorization: 954c00c0-b01a-4863-a75b-1ed238d38f35',
-                            'content'=>$post_string)
-                        );
-                    $stream_context = stream_context_create($context);
-                    $data = file_get_contents($remote_server,FALSE,$stream_context);
-
-                    $contentStr = $fromUsername.' '.$time.' '.$toUsername.' '.$keyword;
+                    $subfix = '@ckan';
+                    if (strrpos($keyword, $subfix) == strlen($keyword) - 5){
+                        $keyword = rtrim($keyword, $subfix);
+                        if (!($keyword=='') & $first){
+                            $first = false;
+                            $post_string = '%7B%22records%22%3A%20%5B%7B%22OpenID%22%3A%20%22'.$fromUsername.'%22%2C%20%22发布时间%22%3A%20%22'.date('Y-m-d H:i',time()).'%22%2C%20%22评论内容%22%3A%20%22'.$keyword.'%22%7D%5D%2C%20%22force%22%3A%20true%2C%20%22method%22%3A%20%22insert%22%2C%20%22resource_id%22%3A%20%2261aab5bb-e5e2-455c-a4eb-77f504df1ce3%22%7D';
+                            $remote_server = 'http://202.121.178.242/api/3/action/datastore_upsert';
+                            $context = array(
+                                'http'=>array(
+                                    'method'=>'POST',
+                                    'header'=>'Authorization: 954c00c0-b01a-4863-a75b-1ed238d38f35',
+                                    'content'=>$post_string)
+                                );
+                            $stream_context = stream_context_create($context);
+                            $data = file_get_contents($remote_server,FALSE,$stream_context);
+                            $contentStr = '我们收到您的消息了: '.$keyword;
+                        }
+                    }else{
+                        $contentStr = $this->simsim($keyword);
+                    }               
                     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, "text", $contentStr);
                     echo $resultStr;
 
@@ -105,6 +115,28 @@ class wechatCallbackapiTest
                         case 'unsubscribe':
                             //$sql = "delete from user_state where fromUsername='$fromUsername'";
                             //$mysql->runSql($sql);
+
+                            $post_string = '%7B%22force%22%3A%20true%2C%20%22filters%22%3A%20%7B%22OpenID%22%3A%20%22'.$fromUsername.'%22%7D%2C%20%22resource_id%22%3A%20%22d7c6b96c-7065-4e1f-9cae-70e2b9914be3%22%7D';
+                            $remote_server = 'http://202.121.178.242/api/3/action/datastore_delete';
+                            $context = array(
+                                'http'=>array(
+                                    'method'=>'POST',
+                                    'header'=>'Authorization: 954c00c0-b01a-4863-a75b-1ed238d38f35',
+                                    'content'=>$post_string)
+                                );
+                            $stream_context = stream_context_create($context);
+                            $data = file_get_contents($remote_server,FALSE,$stream_context);
+
+                            break;
+
+                        case 'CLICK':
+                            $eventKey = trim($postObj->EventKey);
+                            if($eventKey == 'join_us'){
+                                $resultStr = sprintf($webTplHead, $fromUsername, $toUsername, $time, "news", 1);
+                                $resultStr .= sprintf($webTplBody, "集赞总动员","加入我们，为环保点赞", "http://omnilabwechat.sinaapp.com/join/img/top.jpg", "http://omnilabwechat.sinaapp.com/join/join.php?openid=$fromUsername");
+                                $resultStr .= $webTplFoot;
+                                echo $resultStr;
+                            } 
                             break;
 
                         default:
@@ -130,12 +162,24 @@ class wechatCallbackapiTest
                 default:
                     break;
             }
-            $mysql->closeDb();
+            //$mysql->closeDb();
 
         }else {
             
         }
     }
 
+    public function simsim($keyword){
+        $key="51450af1-57bc-4b49-a5f6-2b9e9e87d6b6";
+        $url_simsimi="http://sandbox.api.simsimi.com/request.p?key=".$key."&lc=ch&ft=0.0&text=".$keyword;
+        $json=file_get_contents($url_simsimi);
+        $result=json_decode($json,true);
+        $response=$result['response'];
+        if(!empty($response)){
+            return $response;
+        }else{
+            return '我们收到您的消息了: '.$keyword;
+        }
+    }
 }
 ?>
